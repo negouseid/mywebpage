16cc4a9362bc596c4970c8bc1d36558afd904b95
negouseid <negouseid@gmail.com>
2016-01-04 11:56:17 -0700

    update

--- js/creative.js
+++ js/creative.js
@@ -1,13 +1,8 @@
-/*!
- * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+
 
 (function($) {
-    "use strict"; // Start of use strict
+    "use strict"; 
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -16,18 +11,18 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
+
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     })
 
-    // Closes the Responsive Menu on Menu Item Click
+
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
     });
 
-    // Fit Text Plugin for Main Header
+
     $("h1").fitText(
         1.2, {
             minFontSize: '35px',
@@ -35,14 +30,12 @@
         }
     );
 
-    // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
             top: 100
         }
     })
 
-    // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-})(jQuery); // End of use strict
+})(jQuery);
